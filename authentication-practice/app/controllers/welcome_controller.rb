1fef8112054ca136ca5c76adb66ea7c006fe556f
Ricardo Fleury & Ty Kowalewski <pair+rfleury2.tykowale@devbootcamp.com>
2015-03-24 19:09:12 -0500

    Authentication works and add basic views

--- authentication-practice/app/controllers/welcome_controller.rb
+++ authentication-practice/app/controllers/welcome_controller.rb
@@ -1,8 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-    redirect_to user_path(current_user) if current_user
-    redirect_to new_user_session_path
+    if current_user
+      redirect_to user_path(current_user)
+    else
+      redirect_to new_user_session_path
+    end
   end
 
 end
--- authentication-practice/config/routes.rb
+++ authentication-practice/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :user, only: [:show]
+  resources :users, only: [:show]
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
